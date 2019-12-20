6eca9a04f91288bf36f5945a15043e655c7869f4
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-12-19 19:48:06 -0500

    working

--- run_expr.py
+++ run_expr.py
@@ -11,7 +11,7 @@ def netbricks_sess_setup(trace, nf, epoch):
         print("netbricks session is spawned")
 
         netbricks_sess.send_commands('bash')
-        netbricks_sess.enable_logs(trace + "_" + nf + "--netbricks_" + epoch
+        netbricks_sess.enable_logs(trace + "_" + nf + "--netbricks_" + str(epoch)
                                    + ".log")
         netbricks_sess.send_commands('ssh jethros@tuco')
         netbricks_sess.send_commands('cd /home/jethros/dev/netbricks/experiments')
@@ -19,6 +19,9 @@ def netbricks_sess_setup(trace, nf, epoch):
         return netbricks_sess
     except Exception as err:
         print("Creating screen sessions failed: {}".format(err))
+
+        netbricks_sess.kill()
+
         sys.exit(1)
 
 
@@ -29,11 +32,14 @@ def pktgen_sess_setup(trace):
 
         pktgen_sess.send_commands('bash')
         pktgen_sess.enable_logs(trace + "--pktgen.log")
-        pktgen_sess.send_commands('cd /home/jethros/dev/pktgen-dpdk')
+        pktgen_sess.send_commands('cd /home/jethros/dev/pktgen-dpdk/experiments')
 
         return pktgen_sess
     except Exception as err:
         print("Creating screen sessions failed: {}".format(err))
+
+        pktgen_sess.kill()
+
         sys.exit(1)
 
 
@@ -43,15 +49,19 @@ def sess_destroy(sess):
 
 
 def run_pktgen(trace):
-    cmd_str = "./run_pktgen.sh " + trace
+    cmd_str = "sudo ./run_pktgen.sh " + trace
+    set_port_str= "set 0 rate 100"
+    start_str = "start 0"
+
     pktgen_sess = pktgen_sess_setup(trace)
-    print("Run Pktgen\nTry to run with cmd: {}".format(cmd_str))
-    pktgen_sess.send_commands(cmd_str)
+    # print("Pktgen\nStart with cmd: {}".format(cmd_str))
+    pktgen_sess.send_commands(cmd_str, set_port_str, start_str)
+    print("Pktgen\nRUN pktgen")
     return pktgen_sess
 
 
 def run_netbricks(trace, nf, epoch):
-    cmd_str = "./run_netbricks.sh" + nf
+    cmd_str = "sudo ./run_netbricks.sh " + nf
     netbricks_sess = netbricks_sess_setup(trace, nf, epoch)
     print("Run NetBricks\nTry to run with cmd: {}".format(cmd_str))
     netbricks_sess.send_commands(cmd_str)
