930f5835d5e7f9e1ac55f5c4f86b1e487aecba9e
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-12-23 10:05:53 -0500

    Changed files w/ little update

--- cleanup.sh
+++ cleanup.sh
@@ -20,7 +20,7 @@ if [ -f $HOME/dev/pvn-utils/netbricks.log ]; then
 	mv $HOME/dev/pvn-utils/netbricks.log $HOME/logs/pktgen_logs/$current_date_time
 	printf "Moving just the pvn netbricks logs to the backup logs\n"
 	mkdir -p $HOME/logs/pktgen_logs/$current_date_time
-	mv $HOME/dev/pvn-utils/pktgen.log $HOME/logs/pktgen_logs/$current_date_time
+	mv $HOME/dev/pvn-utils/pktgen*.log $HOME/logs/pktgen_logs/$current_date_time
 	printf "Moving just the pvn pktgen logs to the backup logs\n"
 else
 	printf "nothing to be done?"
