ebb829db21a1515fc4f28cb48c1b0e489176546b
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2020-05-26 21:37:58 -0400

    Changed files w/ little update

--- netbricks_expr/run_netbricks_app.sh
+++ netbricks_expr/run_netbricks_app.sh
@@ -101,7 +101,7 @@ elif [ $2 == "pvn-transcoder-filter" ]; then
 	wait $P1 $P2 $P3 $P4
 
 elif  [ $2 == "app-xcdr_t" ]; then
-	echo "{"setup": $4}\n{"port": $5}" > /home/jethros/setup
+	echo '{"setup": $4}\n{"port": $5}' > /home/jethros/setup
 
 	./home/jethros/dev/pvn-utils/faktory_srv/run_faktory_docker.sh $5 $6 &
 	P0=$!
@@ -119,7 +119,7 @@ elif  [ $2 == "app-xcdr_t" ]; then
 	wait $P0 $P1 $P2 $P3 $P4 $P5
 
 elif  [ $2 == "app-xcdr_g" ]; then
-	echo "{"setup": $4}\n{"port": $5}" > /home/jethros/setup
+	echo '{"setup": $4}\n{"port": $5}' > /home/jethros/setup
 
 	./home/jethros/dev/pvn-utils/faktory_srv/run_faktory_docker.sh $5 $6 &
 	P0=$!
@@ -137,7 +137,7 @@ elif  [ $2 == "app-xcdr_g" ]; then
 	wait $P0 $P1 $P2 $P3 $P4 $P5 $P6
 
 else
-	echo "{"setup": $4}" > /home/jethros/setup
+	echo '{"setup": $4}' > /home/jethros/setup
 	while sleep 1; do ps aux --sort=-%mem | awk 'NR<=10{print $0}'; done | tee $MLOG &
 	P1=$!
 	$BIO_TOP_MONITOR -C | tee $BIO_LOG &
