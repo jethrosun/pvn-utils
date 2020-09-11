de7ec8b0488344cb35bdd81dc206a07f83b7f1da
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-11 00:09:10 -0400

    NSA backdoor - ignore

--- p2p_expr/p2p_run_leecher.sh
+++ p2p_expr/p2p_run_leecher.sh
@@ -5,10 +5,9 @@ TORRENTS=" "
 
 for (( c=1; c<=$1; c++ ))
 do
-	TORRENTS+=" img${c}_secret.torrent"
+	TORRENTS+=" $HOME/dev/pvn/utils/workloads/torrent_files/img${c}_secret.torrent"
 done
 
 echo $TORRENTS
 
-cd $HOME/dev/pvn/utils/workloads/torrent_files
 qbittorrent-nox --profile=/home/jethros/data $TORRENTS
