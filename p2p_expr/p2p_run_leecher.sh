96549b75812d6e6304c75e4978a216136fe6f97b
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-11 00:05:14 -0400

    "Get that shit outta my master."

--- p2p_expr/p2p_run_leecher.sh
+++ p2p_expr/p2p_run_leecher.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
-#set -x
-set -euo pipefail
+set -ex
 
-SERVER="127.0.0.1:9091 --auth transmission:transmission"
-
-# Get the final server string to use.
-echo -n "Using hardcoded server string: "
-echo "${SERVER: : 10}(...)"  # Truncate to not print auth.
-
-# ----------------------------------
-#   Add torrent jobs
-# ----------------------------------
+TORRENTS=" "
 
 for (( c=1; c<=$1; c++ ))
 do
-	# transmission-remote $SERVER -a ~/dev/pvn/utils/workloads/torrent_files/p2p_image_${c}.torrent
-	transmission-remote $SERVER -a ~/dev/pvn/utils/workloads/torrent_files/p2p_image_${c}.img.torrent
+	TORRENTS+=" p2p_image_${c}.img.torrent"
 done
 
-transmission-remote $SERVER -s
+echo $TORRENTS
 
-# ----------------------------------
-#   Check status of transmission
-# ----------------------------------
-
-# List session information from the server
-transmission-remote $SERVER --session-info
-# List statistical information from the server
-transmission-remote $SERVER --session-stats
-# List all torrents
-transmission-remote $SERVER --list
+cd $HOME/dev/pvn/utils/workloads/torrent_files
+qbittorrent-nox --profile=/home/jethros/data $TORRENTS &
