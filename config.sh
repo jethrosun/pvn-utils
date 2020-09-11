f5aa4e29f17896291256c08a4b49cf807b4ec118
Shuwen Jethro Sun <jethro.sun7@gmail.com>
2020-09-11 00:29:20 -0400

    FONDLED THE CODE

--- config.sh
+++ config.sh
@@ -1,50 +1,20 @@
 #!/bin/bash
 set -ex
 
-if [ $HOSTNAME == "tuco" ]; then
+printf "Getting netbricks "
+mkdir -p ~/dev
+rm -rf ~/dev/netbricks
+git clone git@github.com:jethrosun/NetBricks.git -b expr ~/dev/netbricks
+~/dev/netbricks/build.sh
+printf "netbricks building done"
 
-	printf "Getting netbricks "
-	mkdir -p ~/dev
-	rm -rf ~/dev/netbricks
-	git clone git@github.com:jethrosun/NetBricks.git -b expr ~/dev/netbricks
-	cd ~/dev/netbricks
-	./build.sh
-	printf "netbricks building done"
+sudo ~/dev/netbricks/scripts/bind-xl710.sh
 
-	cd ~/dev/netbricks/scripts
-	sudo ./bind-xl710.sh
+# cd ~/dev/netbricks/scripts/tuning
+# sudo ./energy.sh
 
-	# cd ~/dev/netbricks/scripts/tuning
-	# sudo ./energy.sh
-
-	cd ~/dev/pvn/utils
-	git pull
-	sudo ~/dev/pvn/utils/cleanup.sh
-	~/dev/pvn/utils/setup.sh
-	printf "pvn utils done"
-
-
-elif [ $HOSTNAME == "saul" ]; then
-
-	printf "Getting netbricks "
-
-	mkdir -p ~/dev
-	rm -rf ~/dev/netbricks
-	git clone git@github.com:jethrosun/NetBricks.git -b expr ~/dev/netbricks
-	cd ~/dev/netbricks
-	./build.sh
-	printf "netbricks building done"
-
-	cd ~/dev/netbricks/scripts
-	sudo ~/dev/netbricks/scripts/bind-xl710.sh
-
-	# cd ~/dev/netbricks/scripts/tuning
-	# sudo ~/dev/netbricks/scripts/tuning/energy.sh
-
-	cd ~/dev/pvn/utils
-	git pull
-	sudo ~/dev/pvn/utils/cleanup.sh
-	~/dev/pvn/utils/setup.sh
-	printf "pvn utils done"
-
-fi
+cd ~/dev/pvn/utils
+git pull
+sudo ~/dev/pvn/utils/cleanup.sh
+~/dev/pvn/utils/setup.sh
+printf "pvn utils done"
