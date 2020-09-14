b60556008f914967c4ef1f63135e4f784d1de53f
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-14 16:09:31 -0400

    Reinventing the wheel. Again.

--- post_nb_setup.sh
+++ post_nb_setup.sh
@@ -3,8 +3,11 @@ set -x
 
 cd $HOME/dev/pvn/utils
 git pull
+
 $HOME/dev/pvn/utils/setup.sh
 
 sudo $HOME/dev/pvn/utils/cleanup.sh
+
 sudo $HOME/dev/netbricks/scripts/bind-xl710.sh
+
 printf "PVN utils updated\n"
