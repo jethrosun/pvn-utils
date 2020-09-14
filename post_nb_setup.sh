33e9b56174a50421768786fb913421e846444656
Shuwen Jethro Sun <jethro.sun7@gmail.com>
2020-09-14 15:57:56 -0400

    This commit is a lie

--- post_nb_setup.sh
+++ post_nb_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -ex
+set -x
 
 cd $HOME/dev/pvn/utils
 git pull
-sudo $HOME/dev/pvn/utils/cleanup.sh
 $HOME/dev/pvn/utils/setup.sh
 
+sudo $HOME/dev/pvn/utils/cleanup.sh
 sudo $HOME/dev/netbricks/scripts/bind-xl710.sh
 printf "PVN utils updated\n"
