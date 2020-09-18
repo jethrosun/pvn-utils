7c6dd756b2307a573bfc88e050bd853a63fc1cf0
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-18 18:34:37 -0400

    changes

--- p2p_expr/p2p_setup_leecher.sh
+++ p2p_expr/p2p_setup_leecher.sh
@@ -11,20 +11,24 @@ sudo apt-get install deluge -y
 sudo rm -rf ~/qbt_data
 mkdir -p ~/qbt_data
 
-echo "move all the deluge systemd file to where they belong"
-sudo cp systemd_files/deluged.service       /etc/systemd/system/deluged.service
-sudo cp systemd_files/deluge-web.service    /etc/systemd/system/deluge-web.service
-# sudo cp systemd_files/deluged_user.conf
-# sudo cp systemd_files/deluge_web_user.conf
+# fix the gettext error: https://git.deluge-torrent.org/deluge/commit/?h=develop&id=d6c96d629183e8bab2167ef56457f994017e7c85
+sudo sed -i '/gettext.install(I18N_DOMAIN, translations_path, names='ngettext', **kwargs)/c\gettext.install(I18N_DOMAIN, translations_path, names=['ngettext'], **kwargs)' /usr/lib/python3/dist-packages/deluge/i18n/util.py
 
-sudo systemctl enable /etc/systemd/system/deluged.service
-sudo systemctl start deluged
-sudo systemctl status deluged
 
-sudo systemctl enable /etc/systemd/system/deluge-web.service
-sudo systemctl start deluge-web
-sudo systemctl status deluge-web
-
-sudo systemctl daemon-reload
-sudo systemctl restart deluged
-sudo systemctl restart deluge-web
+# echo "move all the deluge systemd file to where they belong"
+# sudo cp systemd_files/deluged.service       /etc/systemd/system/deluged.service
+# sudo cp systemd_files/deluge-web.service    /etc/systemd/system/deluge-web.service
+# # sudo cp systemd_files/deluged_user.conf
+# # sudo cp systemd_files/deluge_web_user.conf
+#
+# sudo systemctl enable /etc/systemd/system/deluged.service
+# sudo systemctl start deluged
+# sudo systemctl status deluged
+#
+# sudo systemctl enable /etc/systemd/system/deluge-web.service
+# sudo systemctl start deluge-web
+# sudo systemctl status deluge-web
+#
+# sudo systemctl daemon-reload
+# sudo systemctl restart deluged
+# sudo systemctl restart deluge-web
