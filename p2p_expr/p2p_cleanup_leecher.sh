867e042799e304bf39d44389a83da9cc50fa0c78
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-27 23:51:55 -0400

    This is a basic implementation that works.

--- p2p_expr/p2p_cleanup_leecher.sh
+++ p2p_expr/p2p_cleanup_leecher.sh
@@ -2,10 +2,22 @@
 set -ex
 
 
-P2P_PID=`ps -eaf | grep p2p_run_leecher | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$P2P_PID" ]]; then
-  echo "killing $P2P_PID"
-  sudo kill -9 $P2P_PID
+BT_PID=`ps -eaf | grep deluged | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$BT_PID" ]]; then
+  echo "killing $BT_PID"
+  sudo kill -9 $BT_PID
+fi
+
+BT_PID=`ps -eaf | grep deluge-gtk | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$BT_PID" ]]; then
+  echo "killing $BT_PID"
+  sudo kill -9 $BT_PID
+fi
+
+BT_PID=`ps -eaf | grep deluge-web | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$BT_PID" ]]; then
+  echo "killing $BT_PID"
+  sudo kill -9 $BT_PID
 fi
 
 BT_PID=`ps -eaf | grep deluged | grep -v grep | awk '{print $2}'`
