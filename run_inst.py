99584b6b20e6e62bae3b0aa65229fcdaa1d6f83c
Shuwen Jethro Sun <jethro.rules@real.com>
2020-11-06 18:47:07 -0500

    less french words

--- run_inst.py
+++ run_inst.py
@@ -203,7 +203,7 @@ def main(expr_list):
                 time.sleep(30)
 
 
-# main(app.rdr_xcdr_tlsv)  # rdr, xcdr
-main(app.rdr_xcdr)  # rdr, xcdr
+main(app.rdr_xcdr_tlsv)  # rdr, xcdr
+# main(app.rdr_xcdr)  # rdr, xcdr
 
 print("All experiment finished {}".format(app.xcdr))
