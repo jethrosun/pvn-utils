34c674fc1c6675d15ec3a9d9cd9e8a9a08012f8f
Shuwen Jethro Sun <jethro.rules@real.com>
2020-11-12 10:00:13 -0500

    Fixed so the code compiles

--- nb_setup.sh
+++ nb_setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -ex
 
-cd "$HOME/dev/pvn/utils" || { cd "$HOME/dev/pvn/"; git clone git@github.com:jethrosun/pvn-utils utils; cd "$HOME/dev/pvn/utils" }
-git pull
+if [ -e "$HOME/dev/pvn/utils" ]; then
+	cd "$HOME/dev/pvn/utils"
+	git pull
+else
+	cd "$HOME/dev/pvn/"; git clone git@github.com:jethrosun/pvn-utils utils;
+fi
 
-cd "$HOME/dev/netbricks" || { cd "$HOME/dev/"; git clone git@github.com:jethrosun/NetBricks.git -b expr netbricks; cd "$HOME/dev/netbricks" }
-git pull
+if [ -e "$HOME/dev/netbricks" ]; then
+	cd "$HOME/dev/netbricks"
+	git pull
+else
+	cd "$HOME/dev/"; git clone git@github.com:jethrosun/NetBricks.git -b expr netbricks;
+fi
 
 "$HOME/dev/netbricks/build.sh"
 printf "netbricks building done\n"
