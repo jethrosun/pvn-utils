69311b7af78f4d223ada8a8e16c4075f908f6549
Shuwen Jethro Sun <jethro.rules@real.com>
2020-12-13 21:22:16 -0500

    derpherp

--- inst_config.py
+++ inst_config.py
@@ -265,8 +265,8 @@ p2p_ext = [ 'app_p2p-ext' ]
 chain_tlsv_rdr = [ 'chain_tlsv_rdr', ]
 
 p2p_chain = [
-    'chain_rdr_p2p',
-    'chain_tlsv_p2p',
+    # 'chain_rdr_p2p',
+    # 'chain_tlsv_p2p',
     'chain_xcdr_p2p',
 ]
 non_p2p_chain = [
