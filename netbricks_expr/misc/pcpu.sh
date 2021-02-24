e097822b4525bf4898036e4f6ca87439d6d40a0c
Shuwen Sun <sun.shuw@northeastern.edu>
2021-02-24 14:06:07 -0500

    epic

--- netbricks_expr/misc/pcpu.sh
+++ netbricks_expr/misc/pcpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "$@"
-top -b -n1 | grep "$@" | awk '{ SUM += $9} END { print SUM }'
+top -b -d 1 -n 1 | grep "$@" | awk '{ SUM += $9} END { print SUM }'
 # ps -eo size,command --sort -size | grep "$@"| awk '{ hr=$1/1024 ; sum +=hr} END {print sum}'
 
 if [ $? -ne 0 ]; then
--- netbricks_expr/misc/pmem.sh
+++ netbricks_expr/misc/pmem.sh
@@ -3,7 +3,7 @@
 # https://unix.stackexchange.com/questions/199315/in-linux-top-command-is-there-any-way-to-keep-track-of-values/199322
 
 echo "$@"
-top -b -n1 | grep "$@" | awk '{ SUM += $10} END { print SUM }'
+top -b -d 1 -n 1 | grep "$@" | awk '{ SUM += $10} END { print SUM }'
 # ps -eo size,command --sort -size | grep "$@"| awk '{ hr=$1/1024 ; sum +=hr} END {print sum}'
 
 if [ $? -ne 0 ]; then
