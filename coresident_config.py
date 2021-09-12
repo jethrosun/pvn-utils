d873506ac12346ab59cca856768c298071615eb4
Shuwen Sun <sun.shuw@northeastern.edu>
2021-09-12 16:45:14 -0400

    Things went wrong...

--- coresident_config.py
+++ coresident_config.py
@@ -88,10 +88,10 @@ xcdr_port_base = 7418
 num_of_epoch = 10
 
 p2p_co = [
-        # 'co_tlsv_rdr_p2p',
-        # 'co_tlsv_p2p_xcdr',
+        'co_tlsv_rdr_p2p',
+        'co_tlsv_p2p_xcdr',
         'co_rdr_xcdr_p2p',
-        # 'co_tlsv_rdr_p2p_xcdr'
+        'co_tlsv_rdr_p2p_xcdr'
         ]
 non_p2p_co = [
         'co_tlsv_rdr_xcdr'
