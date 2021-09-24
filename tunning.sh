b78646be4b96b9b5bf487a6c5e9d07d4d31255a5
Shuwen Sun <sun.shuw@northeastern.edu>
2021-09-24 14:17:51 -0400

    I think now it works

--- tunning.sh
+++ tunning.sh
@@ -10,6 +10,6 @@ echo 0 > /proc/sys/kernel/numa_balancing
 service irqbalance stop
 echo 0 > /sys/kernel/mm/ksm/run
 echo 1-5 > /proc/sys/kernel/watchdog_cpumask
-x86_energy_perf_policy performance
+# x86_energy_perf_policy performance
 modprobe msr
 wrmsr -a 0x620 0x3f3f
