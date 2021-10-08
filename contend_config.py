c34fd64b9c8e7622b0818b9b5e51d49cbe960324
Shuwen Sun <sun.shuw@northeastern.edu>
2021-10-07 20:41:39 -0400

    should work I guess...

--- contend_config.py
+++ contend_config.py
@@ -115,5 +115,5 @@ complete_nuclear_list = ['app_p2p', 'app_p2p-ext']
 p2p_controlled = ['app_p2p-controlled']
 p2p_ext = ['app_p2p-ext']
 
-# nf_list = ['app_tlsv', 'app_rdr', 'app_xcdr', 'app_p2p-controlled']
-nf_list = ['app_p2p-controlled']
+nf_list = ['app_tlsv', 'app_rdr', 'app_xcdr', 'app_p2p-controlled']
+# nf_list = ['app_p2p-controlled']
