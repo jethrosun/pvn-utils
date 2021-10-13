54c3988adb5d8512212e4158588d56a199d9af42
Shuwen Sun <sun.shuw@northeastern.edu>
2021-10-13 10:50:42 -0400

    QuickFix.

--- nb_setup.sh
+++ nb_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+# pull our code first
 if [ -e "$HOME/dev/pvn/utils" ]; then
 	cd "$HOME/dev/pvn/utils"
 	git pull
@@ -15,9 +16,16 @@ else
 	cd "$HOME/dev/"; git clone git@github.com:jethrosun/NetBricks.git -b expr netbricks;
 fi
 
-rustup toolchain install nightly
-rustup default nightly
-rustup override set nightly
+# we will want to always reinstal Rust due to time traveral consequences
+rm -rf $HOME/.rustup
+rm -rf $HOME/.cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+rustup toolchain install nightly-2021-03-15 --allow-downgrade --profile minimal --component rust-src rustfmt
+source $HOME/.cargo/env
+
+
+rustup default nightly-2021-03-15
+rustup override set nightly-2021-03-15
 
 "$HOME/dev/netbricks/build.sh"
 printf "netbricks building done\n"
