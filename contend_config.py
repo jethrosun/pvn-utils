2d6b0ac3e3041d5c6c9ad5c748adfbbe9ce0869f
Shuwen Sun <sun.shuw@northeastern.edu>
2021-10-26 13:39:27 -0400

    I just wanna tell you how I'm feeling

--- contend_config.py
+++ contend_config.py
@@ -138,4 +138,4 @@ p2p_controlled = ['app_p2p-controlled']
 p2p_ext = ['app_p2p-ext']
 
 # nf_list = ['app_p2p-controlled', 'app_tlsv', 'app_rdr', 'app_xcdr']
-nf_list = ['app_rdr', 'app_p2p-controlled']
+nf_list = ['app_p2p-controlled']
