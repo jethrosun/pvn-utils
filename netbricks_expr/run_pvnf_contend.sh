804b9624ef8c1500a4283d23c592c7d4ad624dbb
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-03 10:27:19 -0400

    should work I guess...

--- netbricks_expr/run_pvnf_contend.sh
+++ netbricks_expr/run_pvnf_contend.sh
@@ -38,7 +38,7 @@ LOG_DIR=$HOME/netbricks_logs/$2/$1
 LOG=$LOG_DIR/$3_$4__$5$6$7.log
 TCP_LOG=$LOG_DIR/$3_$4__$5$6$7_tcptop.log
 BIO_LOG=$LOG_DIR/$3_$4__$5$6$7_biotop.log
-TCPLIFE_LOG=$LOG_DIR/$3_$4__$5$6$7_tcplife$5$6$7.log
+# TCPLIFE_LOG=$LOG_DIR/$3_$4__$5$6$7_tcplife$5$6$7.log
 P2P_PROGRESS_LOG=$LOG_DIR/$3_$4__$5$6$7_p2p_progress.log
 FAKTORY_LOG=$LOG_DIR/$3_$4__$5$6$7_faktory.log
 CPU_LOG=$LOG_DIR/$3_$4__$5$6$7_cpu.log
@@ -54,7 +54,7 @@ MEMLOG3=$LOG_DIR/$3_$4__$5$6$7_mem3.log
 
 NETBRICKS_BUILD=$HOME/dev/netbricks/build.sh
 TCP_TOP_MONITOR=/usr/share/bcc/tools/tcptop
-TCP_LIFE_MONITOR=/usr/share/bcc/tools/tcplife
+# TCP_LIFE_MONITOR=/usr/share/bcc/tools/tcplife
 BIO_TOP_MONITOR=/usr/share/bcc/tools/biotop
 
 
@@ -130,13 +130,11 @@ if [ "$2" == 'pvn-transcoder-transform-app' ] || [ "$2" == 'pvn-transcoder-group
 	P10=$!
 	while sleep "$SLEEP_INTERVAL"; do sudo -u jethros taskset -c 5 /home/jethros/dev/pvn/utils/netbricks_expr/misc/pmem.sh contention; done > "$MEMLOG3" &
 	P11=$!
-	sudo taskset -c 5 "$TCP_LIFE_MONITOR" > "$TCPLIFE_LOG" &
-	P12=$!
 	sudo taskset -c 5 "$BIO_TOP_MONITOR" -C > "$BIO_LOG" &
-	P13=$!
+	P12=$!
 	sudo taskset -c 5 "$TCP_TOP_MONITOR" -C > "$TCP_LOG" &
-	P14=$!
-	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14
+	P13=$!
+	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13
 
 elif [ "$2" == "pvn-p2p-transform-app" ] || [ "$2" == "pvn-p2p-groupby-app" ]; then
 	#   $ ./run_pvnf_contend.sh $1=trace $2=nf $3=iter $4=setup $5=cpu $6=mem $7=diskio $8=p2p_type
@@ -218,13 +216,11 @@ elif [ "$2" == "pvn-p2p-transform-app" ] || [ "$2" == "pvn-p2p-groupby-app" ]; t
 	P10=$!
 	while sleep "$SLEEP_INTERVAL"; do sudo -u jethros taskset -c 5 /home/jethros/dev/pvn/utils/netbricks_expr/misc/pmem.sh contention; done > "$MEMLOG3" &
 	P11=$!
-	sudo taskset -c 5 "$TCP_LIFE_MONITOR" > "$TCPLIFE_LOG" &
-	P12=$!
 	sudo taskset -c 5 "$BIO_TOP_MONITOR" -C > "$BIO_LOG" &
-	P13=$!
+	P12=$!
 	sudo taskset -c 5 "$TCP_TOP_MONITOR" -C > "$TCP_LOG" &
-	P14=$!
-	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14
+	P13=$!
+	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13
 
 elif [ "$2" == "pvn-rdr-transform-app" ] || [ "$2" == "pvn-rdr-groupby-app" ]; then
 	# for tlsv and rdr
@@ -294,14 +290,11 @@ elif [ "$2" == "pvn-rdr-transform-app" ] || [ "$2" == "pvn-rdr-groupby-app" ]; t
 	P9=$!
 	while sleep "$SLEEP_INTERVAL"; do sudo -u jethros taskset -c 5 /home/jethros/dev/pvn/utils/netbricks_expr/misc/pmem.sh contention; done > "$MEMLOG3" &
 	P10=$!
-	sudo taskset -c 5 "$TCP_LIFE_MONITOR" > "$TCPLIFE_LOG" &
-	P11=$!
 	sudo taskset -c 5 "$BIO_TOP_MONITOR" -C > "$BIO_LOG" &
-	P12=$!
+	P11=$!
 	sudo  taskset -c 5 "$TCP_TOP_MONITOR" -C > "$TCP_LOG" &
-	P13=$!
-
-	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13
+	P12=$!
+	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12
 
 else
 	# for tlsv and rdr
@@ -361,12 +354,9 @@ else
 	P9=$!
 	while sleep "$SLEEP_INTERVAL"; do sudo -u jethros taskset -c 5 /home/jethros/dev/pvn/utils/netbricks_expr/misc/pmem.sh contention; done > "$MEMLOG3" &
 	P10=$!
-	sudo taskset -c 5 "$TCP_LIFE_MONITOR" > "$TCPLIFE_LOG" &
-	P11=$!
 	sudo taskset -c 5 "$BIO_TOP_MONITOR" -C > "$BIO_LOG" &
-	P12=$!
+	P11=$!
 	sudo  taskset -c 5 "$TCP_TOP_MONITOR" -C > "$TCP_LOG" &
-	P13=$!
-
-	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13
+	P12=$!
+	wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12
 fi
