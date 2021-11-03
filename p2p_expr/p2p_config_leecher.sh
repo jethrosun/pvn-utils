1139721ec1443652e356f4c41dfa53e2723d4ac4
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-03 14:38:54 -0400

    p2p: remove speed setup

--- p2p_expr/p2p_config_leecher.sh
+++ p2p_expr/p2p_config_leecher.sh
@@ -4,7 +4,7 @@ set -ex
 mkdir -p /home/jethros/bt_data/config
 mkdir -p /home/jethros/Downloads
 
-# to properly config deluge, see 
+# to properly config deluge, see
 #
 # 	https://dev.deluge-torrent.org/wiki/UserGuide/BandwidthTweaking
 if [ ! -e /home/jethros/bt_data/config/auth ]; then
@@ -24,18 +24,16 @@ if [ ! -e /home/jethros/bt_data/config/auth ]; then
 	# deluge-console -c /home/jethros/bt_data/config 'config -s cache_size 0'
 	deluge-console -c /home/jethros/bt_data/config "config -s compact_allocation True"
 
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_active_limit 10'
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_active_downloading 10'
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_active_seeding 10'
-
 	# KiB per second? 54000 and 5400
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_download_speed_per_torrent 10000'
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_upload_speed_per_torrent 10000'
-	deluge-console -c /home/jethros/bt_data/config 'config -s max_seed_speed_per_torrent 10000'
 
-	# deluge-console -c /home/jethros/bt_data/config "config -s seed_time_limit 2400"
-	deluge-console -c /home/jethros/bt_data/config "config -s max_download_speed 100000"
-	deluge-console -c /home/jethros/bt_data/config "config -s max_upload_speed 100000"
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_download_speed_per_torrent 10000'
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_upload_speed_per_torrent 10000'
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_seed_speed_per_torrent 10000'
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_active_limit 10'
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_active_downloading 10'
+	# deluge-console -c /home/jethros/bt_data/config 'config -s max_active_seeding 10'
+	# deluge-console -c /home/jethros/bt_data/config "config -s max_download_speed 100000"
+	# deluge-console -c /home/jethros/bt_data/config "config -s max_upload_speed 100000"
 
 	deluge-console -c /home/jethros/bt_data/config "halt"
 fi
