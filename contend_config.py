202a84c01913f0a6ebe10dffc6ff400376523143
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-05 15:09:42 -0400

    these guys are flipped

--- contend_config.py
+++ contend_config.py
@@ -99,8 +99,8 @@ zero = '0'
 setup.append([zero, zero, zero])
 for i in ['1', '2', '3']:
     setup.append([i, zero, zero])
-    # setup.append([zero, i, zero])
-    # setup.append([zero, zero, i])
+    setup.append([zero, i, zero])
+    setup.append([zero, zero, i])
     # setup.append([i, i, zero])
     # setup.append([i, zero, i])
     # setup.append([zero, i, i])
