77cb576ceccfdea538102e9d22667ad92a9ca6ae
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-16 14:22:47 -0500

    Friday 5pm

--- contend_config.py
+++ contend_config.py
@@ -105,14 +105,14 @@ for i in ['1', '2', '3']:
     setup.append([zero, zero, i])
 
 ## combination set
-for i in ['1', '2', '3']:
-    for j in ['1', '2', '3']:
-        for k in ['1', '2', '3']:
-            setup.append([i, j, k])
+# for i in ['1', '2', '3']:
+#     for j in ['1', '2', '3']:
+#         for k in ['1', '2', '3']:
+#             setup.append([i, j, k])
 
 # num_of_epoch = 10
-num_of_epoch = 5
-# num_of_epoch = 3
+# num_of_epoch = 5
+num_of_epoch = 3
 # num_of_epoch = 1
 
 nf_set = {
@@ -124,7 +124,7 @@ nf_set = {
     'app_xcdr': '4',
 }
 
-expr_wait_time = 220  # 180
+expr_wait_time = 250  # 180
 # expr_wait_time = 340  # 300
 # expr_wait_time = 750  # 600
 
@@ -146,5 +146,5 @@ p2p_ext = ['app_p2p-ext']
 
 # nf_list = ['app_p2p-controlled', 'app_rdr', 'app_tlsv', 'app_xcdr']
 # nf_list = ['app_xcdr']
-nf_list = ['app_p2p-controlled']
-# nf_list = ['app_rdr']
+# nf_list = ['app_p2p-controlled']
+nf_list = ['app_rdr']
