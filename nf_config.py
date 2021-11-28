91cf246c735a1d778ac0f233390f0d42e62dca45
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-28 13:54:27 -0500

    these confounded tests drive me nuts

--- nf_config.py
+++ nf_config.py
@@ -158,9 +158,10 @@ xcdr_clean_list = [
 # xcdr_nf_list = ['pvn-transcoder-transform-app', 'pvn-transcoder-groupby-app']
 # xcdr_chain_list = ['pvn-rdr-xcdr-coexist-app', 'pvn-tlsv-xcdr-coexist-app']
 # xcdr_p2p_chain_list = ['pvn-xcdr-p2p-coexist-app']
-
-set_list = ['1', '2', '3', '4', '5', '6']
 # p2p_ext_list = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+
+# set_list = ['1', '2', '3', '4', '5', '6']
+set_list = ['1', ]
 
 # expr is 10 min/600 sec
 expr_wait_time = 220
