26a1820e29f91e8526310d9418c4d19434601823
Shuwen Sun <sun.shuw@northeastern.edu>
2021-12-04 18:23:21 -0500

    bar

--- run_p2p_coresident.py
+++ run_p2p_coresident.py
@@ -174,7 +174,7 @@ def run_expr_p2p_controlled(expr_list):
                 pktgen_sess = pktgen_sess_setup(conf.trace[expr], nf, sending_rate[setup])
                 run_pktgen(pktgen_sess, conf.trace[expr], sending_rate[setup])
                 # epoch from 0 to 9
-                for epoch in range(conf.num_of_epoch):
+                for epoch in range(conf.p2p_num_of_epoch):
                     netbricks_sess = netbricks_sess_setup(conf.trace[expr], nf, epoch, expr)
 
                     leecher1_sess = p2p_sess_setup('flynn', conf.trace[expr], nf, epoch)
