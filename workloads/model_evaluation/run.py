0a0acc48545ff608904f6a84d5829575fb071807
Shuwen Sun <sun.shuw@northeastern.edu>
2021-12-19 16:11:23 -0500

    checkpoint: wip fine grained workload

--- workloads/model_evaluation/run.py
+++ workloads/model_evaluation/run.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
 import random
+import pickle
+import os
 
-nf_list = ['tlsv', 'rdr', 'xcdr', 'p2p']
 
-nf_workload = []
-for i in range(10):
-    nf_workload.append(random.choice(nf_list))
-print("nf workload\n", nf_workload)
+if os.path.exists("ordered_runs.pickle"):
+    print("loading ordered runs from pickle file\n")
 
-ordered_runs = {}
-for i in range(5):
-    random.shuffle(nf_workload)
-    ordered_runs[i] = nf_workload
-print("ordered runs\n", ordered_runs)
+    with open('ordered_runs.pickle', 'rb') as handle:
+        ordered_runs= pickle.load(handle)
+    print("ordered runs\n", ordered_runs)
+
+else:
+    print("not find pickle file, generating one now...\n")
+    nf_list = ['tlsv', 'rdr', 'xcdr', 'p2p']
+
+    nf_workload = []
+    for i in range(10):
+        nf_workload.append(random.choice(nf_list))
+    print("nf workload\n", nf_workload)
+
+    ordered_runs = {}
+    for i in range(5):
+        random.shuffle(nf_workload)
+        ordered_runs[i] = nf_workload
+    print("ordered runs\n", ordered_runs)
+
+    with open('ordered_runs.pickle', 'wb') as handle:
+        pickle.dump(ordered_runs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def get_nodes(num_of_nodes):
@@ -73,8 +88,8 @@ def rand_placement(num_of_nodes, ordered_runs):
     return workload
 
 
-nodes = rand_placement(3, ordered_runs)
-print("rand placement:\n", nodes)
+rand_nodes = rand_placement(3, ordered_runs)
+print("rand placement:\n", rand_nodes)
 
 
 def least_load_placement(num_of_nodes, ordered_runs):
@@ -89,9 +104,25 @@ def least_load_placement(num_of_nodes, ordered_runs):
     return workload
 
 
-nodes = least_load_placement(3, ordered_runs)
-print("least load placement:\n", nodes)
+llf_nodes = least_load_placement(3, ordered_runs)
+print("least load placement:\n", llf_nodes)
 
 
 def fancy_placement(num_of_nodes, nf_workload):
     nodes = get_nodes(num_of_nodes)
+
+
+def de_dup_runs(r_dicts):
+    nodes = []
+    for run_nodes in r_dicts:
+        for iter in run_nodes:
+            for node in run_nodes[iter]:
+                load =  run_nodes[iter][node]
+                if load in nodes:
+                    print(iter, node, load, "already exists in nodes!")
+                else:
+                    nodes.append(load)
+    return nodes
+
+final_nodes = de_dup_runs([rand_nodes, llf_nodes])
+print("final nodes after dedup are:", final_nodes)
