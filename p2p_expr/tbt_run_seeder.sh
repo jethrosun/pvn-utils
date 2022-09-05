40947a3e103d9d0de34d3a939e009c4da4f3987b
Shuwen Sun <sun.shuw@northeastern.edu>
2022-09-05 11:37:58 -0400

    seeder update

--- p2p_expr/tbt_run_seeder.sh
+++ p2p_expr/tbt_run_seeder.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-#set -x
+
+# https://github.com/ashmckenzie/percheron-torrent/blob/b93a1a1df5614d448ec6880777a4fa7a1aa1d504/seeder/bin/seed.sh
 set -euo pipefail
 
+
 # Server string: "host:port --auth username:password"
-SERVER="127.0.0.1:9091 --auth transmission:transmission"
+# SERVER="127.0.0.1:9091 --auth transmission:transmission"
+SERVER="--private --tracker http://10.200.111.125:8080/announce"
 
 # Get the final server string to use.
 echo -n "Using hardcoded server string: "
+# echo "${SERVER: : 10}(...)"  # Truncate to not print auth.
 echo "${SERVER: : 10}(...)"  # Truncate to not print auth.
 
 # ----------------------------------
 #   Check status of transmission
 # ----------------------------------
 
-for (( c=1; c<=10; c++ ))
+for core in {1..5}
 do
-	transmission-remote --torrent=/home/jethros/data/p2p_image_${c}.torrent \
-                -a p2p_image_${c}.torrent --verify \
-                --download-dir=/home/jethros/data --start
+        echo "Core $core "
+	for i in {0..12}
+	do
+                transmission-remote --torrent=/home/jethros/data/p2p_core_${core}_image_${c}.torrent \
+                        -a p2p_image_${c}.torrent --verify \
+                        --download-dir=/home/jethros/data --start
+	done
 done
 
 # transmission-daemon -c ~/data
---
+++ p2p_expr/tbt_setup_seeder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ex
+
+# seeder setup
+SERVER="--private --tracker http://10.200.111.125:8080/announce"
+
+# https://www.cyberciti.biz/faq/howto-create-lage-files-with-dd-command/
+# https://superuser.com/questions/470949/how-do-i-create-a-1gb-random-file-in-linux
+sudo rm -rf ~/data
+mkdir -p ~/data
+mkdir -p ~/torrents
+cd ~/data
+
+# the max number is 13 for now, so it is 5 cores * 13 images
+# https://www.reddit.com/r/torrents/comments/8likab/how_to_create_a_trackerless_torrent_using_the_dht/
+for core in {1..5}
+do
+	echo "Core $core "
+	for i in {0..12}
+	do
+		echo "Making $i images"
+		sudo fallocate -l 1G p2p_core_${core}_image_${i}.img
+		sudo dd if=/dev/random of=p2p_core_${core}_image_${i}.img bs=1 count=0 seek=1G
+		stat p2p_core_${core}_image_${i}.img
+
+		transmission-create $SERVER p2p_core_${core}_image_${i}.img
+		cp p2p_core_${core}_image_${i}.torrent ~/torrents
+		chmod 644 ~/torrents/p2p_core_${core}_image_${i}.torrent
+
+		exec /usr/bin/aria2c --seed-ratio=0.0 -V -d . p2p_core_${core}_image_${i}.torrent
+	done
+done
+
+
+echo "To create torrents and start seeding them, a qBitTorrent GUI must be used."
