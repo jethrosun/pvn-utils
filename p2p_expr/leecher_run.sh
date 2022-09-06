9aae43e89ae3de82625fc567680505b84d04e1f4
Shuwen Sun <sun.shuw@northeastern.edu>
2022-09-06 14:03:34 -0400

    fix

--- p2p_expr/leecher_run.sh
+++ p2p_expr/leecher_run.sh
@@ -4,4 +4,5 @@ set -exuo pipefail
 # https://github.com/ashmckenzie/percheron-torrent/blob/b93a1a1df5614d448ec6880777a4fa7a1aa1d504/seeder/bin/seed.sh
 # http://aria2.github.io/manual/en/html/aria2c.html
 
-cd ~/torrents && qbittorrent-nox *
+cd ~/data/downloads
+aria2c ~/torrents/*.torrent
