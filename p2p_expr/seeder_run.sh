38f3efb424e263874762f0934fc19ebf83873542
Shuwen Sun <sun.shuw@northeastern.edu>
2022-09-06 14:25:37 -0400

    Don't Ask Me, I Have No Idea Why This Works Either

--- p2p_expr/seeder_run.sh
+++ p2p_expr/seeder_run.sh
@@ -4,4 +4,9 @@ set -exuo pipefail
 # https://github.com/ashmckenzie/percheron-torrent/blob/b93a1a1df5614d448ec6880777a4fa7a1aa1d504/seeder/bin/seed.sh
 # http://aria2.github.io/manual/en/html/aria2c.html
 
+# cd ~/data/downloads
+# aria2c --seed-ratio=0.0 --listen-port=9000 --bt-enable-lpd --bt-external-ip=10.200.111.125 --enable-peer-exchange -V -d . ~/torrents/*.torrent
+
+
 cd ~/torrents && qbittorrent-nox *
+
