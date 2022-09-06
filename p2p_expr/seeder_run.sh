a4bc7acb99c172006903cec039b67d8b35b05d2b
Shuwen Sun <sun.shuw@northeastern.edu>
2022-09-06 13:48:08 -0400

    Copy pasta fail. still had a instead of a

--- p2p_expr/seeder_run.sh
+++ p2p_expr/seeder_run.sh
@@ -4,20 +4,17 @@ set -exuo pipefail
 # https://github.com/ashmckenzie/percheron-torrent/blob/b93a1a1df5614d448ec6880777a4fa7a1aa1d504/seeder/bin/seed.sh
 # http://aria2.github.io/manual/en/html/aria2c.html
 
-
-# echo "To create torrents and start seeding them, a qBitTorrent GUI must be used."
-
-for core in {1..5}
-do
-        echo "Core $core "
-	for i in {0..12}
-	do
-		transmission-remote --add ~/torrents/p2p_core_${core}_image_${i}.torrent --verify --start
-                # transmission-remote --torrent=/home/jethros/data/p2p_core_${core}_image_${i}.torrent \
-                #         -a p2p_core_${core}_image_${i}.torrent --verify \
-                #         --download-dir=/home/jethros/data --start
-	done
-done
+# for core in {1..5}
+# do
+#         echo "Core $core "
+# 	for i in {0..12}
+# 	do
+# 		transmission-remote --add ~/torrents/p2p_core_${core}_image_${i}.torrent --verify --start
+#                 # transmission-remote --torrent=/home/jethros/data/p2p_core_${core}_image_${i}.torrent \
+#                 #         -a p2p_core_${core}_image_${i}.torrent --verify \
+#                 #         --download-dir=/home/jethros/data --start
+# 	done
+# done
 #
 # # transmission-daemon -c ~/data
 # # ----------------------------------
