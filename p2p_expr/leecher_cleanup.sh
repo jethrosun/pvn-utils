9acb20b6e2c943ed5462feb58216156af91b3aff
Shuwen Sun <sun.shuw@northeastern.edu>
2022-10-23 21:05:04 -0400

    copy and paste is not a design pattern

--- p2p_expr/leecher_cleanup.sh
+++ p2p_expr/leecher_cleanup.sh
@@ -28,4 +28,4 @@ sudo chmod -R 775 /home/jethros/bt_data
 sudo -u jethros deluged -c /home/jethros/bt_data/config
 sudo -u jethros deluge-console -c /home/jethros/bt_data/config halt
 
-sudo -u jethro cp /home/jethros/dev/pvn/p2p-builder/scripts/core.conf /home/jethros/bt_data/config/core.conf
+sudo -u jethros cp /home/jethros/dev/pvn/p2p-builder/scripts/core.conf /home/jethros/bt_data/config/core.conf
