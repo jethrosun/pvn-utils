16ea9b231132ba8e6affb986ce32a57bc61ad25d
Shuwen Sun <sun.shuw@northeastern.edu>
2022-11-01 23:00:21 -0400

    fix

--- netbricks_expr/run_udf_contend.sh
+++ netbricks_expr/run_udf_contend.sh
@@ -119,8 +119,8 @@ while sleep 5; do
 done &
 pids="$pids $!"
 
-# "$NETBRICKS_BUILD" run "$2" -f "$TMP_NB_CONFIG" > "$LOG" &
-# pids="$pids $!"
+"$NETBRICKS_BUILD" run "$2" -f "$TMP_NB_CONFIG" > "$LOG" &
+pids="$pids $!"
 
 core_id=3
 
