08da2d20aeb05e788cb4535f2e6854a20456a222
Shuwen Sun <sun.shuw@northeastern.edu>
2022-11-04 16:27:42 -0400

    I expected something different.

--- nf_config.py
+++ nf_config.py
@@ -92,11 +92,12 @@ for i in ['1', '2', '3']:
 #batch_list = ['400019', '664673', '1374946']
 batch_list = ['664673']
 
-# schedule_list = [ 'rand' ]
-# schedule_list = [ 'resrc_pining_23_', 'resrc_pining_32_', 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311' ]
-#
-schedule_list = [ 'rand', 'resrc_pining_23_', 'resrc_pining_32_', 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311' ]
+# schedule_list = [ 'rand', 'resrc_pining_23_', 'resrc_pining_32_', 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311' ]
 # schedule_list = [ 'rand', 'resrc_pining_311', 'resrc_pining_23_', 'resrc_pining_32_', 'resrc_pining_122' ]
 # schedule_list = [ 'rand', 'resrc_central', 'profile' ]
 
 sched_num_of_epoch = 1
+
+schedule_list = [ 'rand' ]
+# schedule_list = [ 'resrc_pining_23_', 'resrc_pining_32_', 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311' ]
+#
