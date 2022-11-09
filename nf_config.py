5790cbe87e1f9ea6750afffb455c9c9c12107f5d
Shuwen Sun <sun.shuw@northeastern.edu>
2022-11-09 17:17:46 -0500

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- nf_config.py
+++ nf_config.py
@@ -96,7 +96,7 @@ batch_list = ['664673']
 
 sched_num_of_epoch = 1
 
-# schedule_list = ['rand', 'resrc_pining_23_', 'resrc_pining_32_',
-#                  'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311']
+schedule_list = ['rand', 'resrc_pining_23_', 'resrc_pining_32_',
+                 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311']
 # schedule_list = [ 'rand', 'resrc_central' ]
-schedule_list = ['rand']
+# schedule_list = ['rand']
