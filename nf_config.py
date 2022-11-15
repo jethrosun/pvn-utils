3f9b4c6a6bd817c28a5892c51c10f9b8fb9bbc9d
Shuwen Sun <sun.shuw@northeastern.edu>
2022-11-14 22:03:30 -0500

    clarify further the brokenness of C++. why the fuck are we using C++?

--- nf_config.py
+++ nf_config.py
@@ -96,7 +96,8 @@ batch_list = ['664673']
 
 sched_num_of_epoch = 1
 
-schedule_list = ['rand', 'resrc_pining_23_', 'resrc_pining_32_',
-                 'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311']
-# schedule_list = [ 'rand', 'resrc_central' ]
+# schedule_list = ['rand', 'resrc_pining_23_', 'resrc_pining_32_',
+#                  'resrc_pining_41_', 'resrc_pining_122', 'resrc_pining_311']
+schedule_list = ['resrc_central', 'rand',
+                 'resrc_pining_122', 'resrc_pining_41_']
 # schedule_list = ['rand']
