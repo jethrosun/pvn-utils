091866fb2613a3914b7f89e92aa96c4740355f9e
Shuwen Sun <sun.shuw@northeastern.edu>
2023-01-23 23:57:37 -0500

    better ignores

---
+++ p2p_expr/seeder_setup_50_rc.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -e
+
+# sudo iptables -I INPUT -p tcp --dport 9000 -j ACCEPT
+# sudo ufw allow out 9000/tcp
+
+# https://www.cyberciti.biz/faq/howto-create-lage-files-with-dd-command/
+# https://superuser.com/questions/470949/how-do-i-create-a-1gb-random-file-in-linux
+rm -rf ~/data
+rm -rf ~/torrents
+mkdir -p ~/data/downloads
+mkdir -p ~/data/Downloads
+mkdir -p ~/torrents
+
+cd ~/data/downloads
+# the max number is 13 for now, so it is 5 cores * 13 images
+# https://www.reddit.com/r/torrents/comments/8likab/how_to_create_a_trackerless_torrent_using_the_dht/
+for core in {1..1}
+do
+	echo "Core $core "
+	# 101 images
+	for i in {0..85}
+	do
+		echo "Making $i images"
+		sudo fallocate -l 1G p2p_core_${core}_image_${i}.img
+		sudo dd if=/dev/random of=p2p_core_${core}_image_${i}.img bs=1 count=0 seek=1G
+
+		# stat p2p_core_${core}_image_${i}.img
+		sudo chmod 644 p2p_core_${core}_image_${i}.img
+
+		# make torrents
+		# transmission-create -o ~/torrents/p2p_core_${core}_image_${i}.torrent --private --tracker http://10.200.111.125:9000/announce p2p_core_${core}_image_${i}.img
+		mktorrent -v -p -a http://10.200.111.125:9000/announce -o ~/torrents/p2p_core_${core}_image_${i}.torrent p2p_core_${core}_image_${i}.img
+
+		# cp p2p_core_${core}_image_${i}.img.torrent ~/torrents
+		chmod 644 ~/torrents/p2p_core_${core}_image_${i}.torrent
+
+	done
+done
+
+for core in {2..3}
+do
+	echo "Core $core "
+	# 101 images
+	for i in {0..50}
+	do
+		echo "Making $i images"
+		sudo fallocate -l 1G p2p_core_${core}_image_${i}.img
+		sudo dd if=/dev/random of=p2p_core_${core}_image_${i}.img bs=1 count=0 seek=1G
+
+		# stat p2p_core_${core}_image_${i}.img
+		sudo chmod 644 p2p_core_${core}_image_${i}.img
+
+		# make torrents
+		# transmission-create -o ~/torrents/p2p_core_${core}_image_${i}.torrent --private --tracker http://10.200.111.125:9000/announce p2p_core_${core}_image_${i}.img
+		mktorrent -v -p -a http://10.200.111.125:9000/announce -o ~/torrents/p2p_core_${core}_image_${i}.torrent p2p_core_${core}_image_${i}.img
+
+		# cp p2p_core_${core}_image_${i}.img.torrent ~/torrents
+		chmod 644 ~/torrents/p2p_core_${core}_image_${i}.torrent
+
+	done
+done
+
+
+for core in {4..4}
+do
+	echo "Core $core "
+	# 101 images
+	for i in {0..120}
+	do
+		echo "Making $i images"
+		sudo fallocate -l 1G p2p_core_${core}_image_${i}.img
+		sudo dd if=/dev/random of=p2p_core_${core}_image_${i}.img bs=1 count=0 seek=1G
+
+		# stat p2p_core_${core}_image_${i}.img
+		sudo chmod 644 p2p_core_${core}_image_${i}.img
+
+		# make torrents
+		# transmission-create -o ~/torrents/p2p_core_${core}_image_${i}.torrent --private --tracker http://10.200.111.125:9000/announce p2p_core_${core}_image_${i}.img
+		mktorrent -v -p -a http://10.200.111.125:9000/announce -o ~/torrents/p2p_core_${core}_image_${i}.torrent p2p_core_${core}_image_${i}.img
+
+		# cp p2p_core_${core}_image_${i}.img.torrent ~/torrents
+		chmod 644 ~/torrents/p2p_core_${core}_image_${i}.torrent
+
+	done
+done
+
+for core in {5..5}
+do
+	echo "Core $core "
+	# 101 images
+	for i in {0..160}
+	do
+		echo "Making $i images"
+		sudo fallocate -l 1G p2p_core_${core}_image_${i}.img
+		sudo dd if=/dev/random of=p2p_core_${core}_image_${i}.img bs=1 count=0 seek=1G
+
+		# stat p2p_core_${core}_image_${i}.img
+		sudo chmod 644 p2p_core_${core}_image_${i}.img
+
+		# make torrents
+		# transmission-create -o ~/torrents/p2p_core_${core}_image_${i}.torrent --private --tracker http://10.200.111.125:9000/announce p2p_core_${core}_image_${i}.img
+		mktorrent -v -p -a http://10.200.111.125:9000/announce -o ~/torrents/p2p_core_${core}_image_${i}.torrent p2p_core_${core}_image_${i}.img
+
+		# cp p2p_core_${core}_image_${i}.img.torrent ~/torrents
+		chmod 644 ~/torrents/p2p_core_${core}_image_${i}.torrent
+
+	done
+done
+
+sudo chown -R jethros:jethros .
+
