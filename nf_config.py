ca751278f1a0836057ee1e4ad0976e2d982fd612
Shuwen Sun <sun.shuw@northeastern.edu>
2023-03-07 15:49:01 -0500

    one

--- nf_config.py
+++ nf_config.py
@@ -145,8 +145,8 @@ sched_num_of_epoch = 1
 # batch_list = [ '400019_10', '1374946_10', '664673_10',]
 
 
-# schedule_list = ['rand']
-schedule_list = ['profile_w_rand']
+schedule_list = ['rand']
+# schedule_list = ['profile_w_rand']
 # schedule_list = ['resrc_central']
 batch_list = ['400019_50', '1374946_50', '664673_50', ]
 #batch_list = ['1374946_30', '664673_30', '400019_30', '400019_10', '1374946_10', '664673_10',]
